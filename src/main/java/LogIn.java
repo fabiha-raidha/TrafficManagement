e1a21c56d1fc0fd31e1ec70ec47083b596c7f5f5
Your Name <frkhan915@gmail.com>
2025-08-02 13:32:31 +0600

    Log In FXML and class updated

--- src/main/java/LogIn.java
+++ src/main/java/LogIn.java
@@ -1,2 +1,18 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
 public class LogIn {
+
+    @FXML
+    private TextField passwordTF;
+
+    @FXML
+    private TextField usernameTF;
+
+    @FXML
+    void onLogInButtonClicked(MouseEvent event) {
+
+    }
+
 }
