29c481c498ac6eb2d780707a14d7031be21dd3f3
Your Name <frkhan915@gmail.com>
2025-08-05 21:04:03 +0600

    Hello Application updated

--- src/main/java/oop/trafficproject/HelloApplication.java
+++ src/main/java/oop/trafficproject/HelloApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/GeneralFxml/hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("LogIn.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Log In");
         stage.setScene(scene);
