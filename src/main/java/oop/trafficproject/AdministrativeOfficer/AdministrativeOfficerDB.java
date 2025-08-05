4bb92239a832a4ff4cc68ba6bf3a16b9e7c649e0
Your Name <frkhan915@gmail.com>
2025-08-05 21:04:38 +0600

    AdministrativeOfficerDB java file is created and updated

--- src/main/java/oop/trafficproject/AdministrativeOfficer/AdministrativeOfficerDB.java
+++ src/main/java/oop/trafficproject/AdministrativeOfficer/AdministrativeOfficerDB.java
@@ -1,4 +1,60 @@
 package oop.trafficproject.AdministrativeOfficer;
 
-public class AdministrativeOfficerDB {
+import oop.trafficproject.User;
+
+import java.time.LocalDate;
+
+public class AdministrativeOfficerDB extends User {
+    private int employeeId;
+    private String department;
+    private LocalDate doj;
+
+    public AdministrativeOfficerDB(String gender, LocalDate dob, String name, String email, String phone, String password, int employeeId, String department, LocalDate doj) {
+        super (employeeId, gender, dob);
+        this.employeeId = employeeId;
+        this.department = department;
+        this.doj = doj;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public LocalDate getDoj(){
+        return doj;
+    }
+
+    public void sendProfileStatusRequest(){
+
+    }
+
+    public void assignZone(User officer, String zone) {
+        officer.setAssignedZone(zone);
+        System.out.println("Assigned zone" + zone + "to" + officer.getEmpId());
+    }
+
+    @Override
+    public boolean updateProfile() {
+        System.out.println("Administrative profile is updated.");
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "AdministrativeOfficerDB{" +
+                "employeeId=" + employeeId +
+                ", name='" + name + '\'' +
+                ", department='" + department + '\'' +
+                ", doj=" + doj +
+                ", designation='" + designation + '\'' +
+                '}';
+    }
 }
