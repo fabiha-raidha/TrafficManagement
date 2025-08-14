df5025728d69a10989ffda9a07e6bd248cb67c17
Tanjil-01 <2420829@iub.edu.bd>
2025-08-14 14:33:11 +0600

    Tanjil-01

---
+++ src/main/java/oop/trafficproject/CitizenReporterDashboard01Controller.java
@@ -0,0 +1,48 @@
+package oop.trafficproject;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
+public class CitizenReporterDashboard01Controller
+{
+    @javafx.fxml.FXML
+    private Label nameShowLabelFXID;
+    @javafx.fxml.FXML
+    private Label idShowLabelFXID;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void LocalUpdatesOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void EditOrCancelAReportOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void GiveaFeedbackOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void ReportAnissueOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void MarkHazardOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void TrackReportOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void FeedbackHistoryOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void MyReportsOnAction(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/oop/trafficproject/RoadMaintenanceOfficerDashboard01Controller.java
@@ -0,0 +1,49 @@
+package oop.trafficproject;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
+// oop.trafficproject.CitizenReporterDashboard01Controller //
+public class RoadMaintenanceOfficerDashboard01Controller
+{
+    @javafx.fxml.FXML
+    private Label nameShowLabelFXID;
+    @javafx.fxml.FXML
+    private Label idShowLabelFXID;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void AssignToCrewOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void LogDelayOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void CitizenReportsOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void AssignedZonesOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void DailyLogOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void CitizenFeedbackOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void MaintenanceDashBoardOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void UpdateStatusOnAction(ActionEvent actionEvent) {
+    }
+}
