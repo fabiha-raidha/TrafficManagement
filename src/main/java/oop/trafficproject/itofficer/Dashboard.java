6107a00d03a33c193f1921f56b7ff181780e4cab
adibaeisha <adibaeisha09@gmail.com>
2025-08-16 05:05:18 +0600

    Added User 3 and 4

---
+++ src/main/java/oop/trafficproject/itofficer/Dashboard.java
@@ -0,0 +1,7 @@
+package oop.trafficproject.itofficer;
+
+public class Dashboard
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
---
+++ src/main/java/oop/trafficproject/trafficupdate_genuser/Dashboard2.java
@@ -0,0 +1,7 @@
+package oop.trafficproject.trafficupdate_genuser;
+
+public class Dashboard2
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
